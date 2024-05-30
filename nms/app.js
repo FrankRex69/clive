00099c9d14a39bafc37e7e05dc263160dc8ca3bf
FrankRex69 <francesco.re69@gmail.com>
2024-05-30 11:11:15 +0200

    feat: setup nms

--- nms/app.js
+++ nms/app.js
@@ -17,7 +17,7 @@ require('dotenv').config();
 // }
 
 port_nms_rtmp=1941;
-port_nms_http=8471;
+port_nms_http=8015;
 port_nms_https=8471;
  
 const config = {
@@ -31,12 +31,12 @@ const config = {
   http: {
     port: port_nms_http,
     allow_origin: '*'
-   }
-  // https: {
-  //   port: port_nms_https,
-  //   key: fs.readFileSync('./cert/privkey.pem'),
-  //   cert: fs.readFileSync('./cert/cert.pem')
-  // }
+   },
+  https: {
+    port: port_nms_https,
+    key: 'cert/privkey.pem',
+    cert: 'cert/cert.pem'
+  }
 };
  
 var nms = new NodeMediaServer(config)
